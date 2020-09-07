6d01126890684944cddf6f7832a193c29a1921a0
manggong <jihwank1995@gmail.com>
2020-09-07 16:50:01 +0900

    first commit

---
+++ src/main/java/kr/co/example/Main.java
@@ -0,0 +1,12 @@
+package kr.co.example;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.debug("Hello World");
+    }
+}
