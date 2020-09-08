6ea10598779d9dd3f59453e8006da082f6f5bb10
manggong <jihwank1995@gmail.com>
2020-09-08 22:45:49 +0900

    Apply JDBC

--- src/main/java/kr/co/example/Main.java
+++ src/main/java/kr/co/example/Main.java
@@ -3,10 +3,25 @@ package kr.co.example;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 
+import java.sql.*;
+
 public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         logger.debug("Hello World");
+
+        Connection connection = null;
+        try {
+            Class.forName("org.h2.Driver");
+            String url = "jdbc:h2:~/test;MODE=MySQL;";
+            connection = DriverManager.getConnection(url, "sa", "");
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from member");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
