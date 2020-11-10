c2e83578323564322d09444781df8c0251647086
manggong <jihwank1995@gmail.com>
2020-11-10 22:25:21 +0900

    Add logback

--- src/main/java/kr/co/example/Main.java
+++ src/main/java/kr/co/example/Main.java
@@ -1,7 +1,13 @@
 package kr.co.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hello");
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
     }
 }
+
