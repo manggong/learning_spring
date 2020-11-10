0d3a578281e9daf3a214ce343ea91a3259782962
manggong <jihwank1995@gmail.com>
2020-11-10 21:58:26 +0900

    init Project

--- src/main/java/kr/co/example/Main.java
+++ src/main/java/kr/co/example/Main.java
@@ -1,27 +1,7 @@
 package kr.co.example;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
-import java.sql.*;
-
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
-    public static void main(String[] args) throws SQLException {
-        logger.debug("Hello World");
-
-        Connection connection = null;
-        try {
-            Class.forName("org.h2.Driver");
-            String url = "jdbc:h2:~/test;MODE=MySQL;";
-            connection = DriverManager.getConnection(url, "sa", "");
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from member");
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args){
+        System.out.println("hello");
     }
 }
