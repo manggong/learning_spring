889124c813305f8c98873f486a3f0665ab1b4c0a
manggong <jihwank1995@gmail.com>
2020-11-10 23:13:46 +0900

    Apply JDBC

--- src/main/java/kr/co/example/Main.java
+++ src/main/java/kr/co/example/Main.java
@@ -3,11 +3,35 @@ package kr.co.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.*;
+
 public class Main {
-    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         logger.info("Hello world");
+    }
+
+    Connection connection = null;
+    Statement statement = null;
+    try {
+        Class.forName("org.h2.Driver");
+        String url = "jdbc:h2:~/test;MODE=MySQL";
+        connection = DriverManager.getConnection(url, "sa", "");
+        statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from member");
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String username = resultSet.getString("username");
+            String password = resultSet.getString("password");
+
+            logger.info("id: " + id);
+        }
+    } catch(ClassNotFoundException e) {
+        e.printStackTrace();
+    } catch(SQLException e) {
+        e.printStackTrace();
     }
 }
 
